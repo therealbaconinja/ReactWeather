998fe4085f26a40c9fde88a27cee01fc98196135
rmilbrath <ryan@justuno.com>
2017-06-22 07:57:03 -0700

    Foundation styling cont.

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,20 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>This is the examples page.</p>
-    </div>    
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out: </p>
+      <ol>
+        <li>
+          <Link to="/?location=Seattle">Seattle, WA</Link>
+        </li>
+        <li>
+          <Link to="/?location=Barcelona">Barcelona, Spain</Link>
+        </li>
+      </ol>
+    </div>
   )
 }
 
