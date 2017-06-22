aec123724ffb1878d043d9118a91babc022d5537
rmilbrath <ryan@justuno.com>
2017-06-22 08:39:22 -0700

    add About links

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -26,8 +26,48 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>This is the about page.</p>
+      <h1 className="text-center">About</h1>
+      <p>Welcome to my first React App! The following resources helped make this happen: </p>
+      <h4><a href="http://www.udemy.com" target="_blank">Udemy.com</a></h4>
+      <p>Change your life through learning!</p>
+      <h4><a href="https://github.com/therealbaconinja/ReactWeather" target="_blank">My App on Github.com</a></h4>
+      <p>Teach a person to fish and they might make you dinner!</p>
+      <h4><a href="http://www.heroku.com" target="_blank">Heroku.com</a></h4>
+      <p>Get your head IN the Cloud!</p>
+      <h4><a href="http://www.openweathermap.org" target="_blank">Open Weather Map</a></h4>
+      <p>What will those Clouds do today?!</p>
+
+      {/* <div className="card">
+        <div className="card-divider">
+          Learning
+        </div>
+        <img src="https://www.udemy.com/staticx/udemy/images/v5/logo-green.svg"/>
+        <div className="card-section">
+          <h4><Link to="http://www.udemy.com">Udemy.com</Link></h4>
+          <p>Change your life through learning!</p>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-divider">
+          Sharing
+        </div>
+        <img src="https://s3.amazonaws.com/media.eremedia.com/uploads/2017/01/23143044/github4.jpg"/>
+        <div className="card-section">
+          <h4><Link to="https://github.com/therealbaconinja/ReactWeather">This ReactWeather on Github.com</Link></h4>
+          <p>Teach a person to fish and they might make you dinner!</p>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-divider">
+          Hosting
+        </div>
+        <img src="http://perfect.org/images/heroku-logo.jpg"/>
+        <div className="card-section">
+          <h4><Link to="http://www.heroku.com">Heroku.com</Link></h4>
+          <p>Get your head IN the Cloud!</p>
+        </div>
+      </div> */}
+
     </div>
   )
 };
