7c50692283e8d72f6d9dad92c36bc2961b8dd02e
rmilbrath <ryan@justuno.com>
2017-06-29 08:01:04 -0700

    add nav search

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -15,9 +15,9 @@ var Weather = React.createClass({
 
     this.setState ({
       isLoading: true,
-      errorMessage: undefined,
-      location: undefined,
-      temp: undefined
+      errorMessage: undefined
+      //location: undefined,
+      //temp: undefined
     });
 
     openWeatherMap.getTemp(location).then(function (temp) {
@@ -34,8 +34,17 @@ var Weather = React.createClass({
     });
   },
   componentDidMount: function () {
-    //get location query param from react-router    
+    //get location query param from react-router
     var location = this.props.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      //remove location querystring after weather search
+      window.location.hash = '#/';
+    }
+  },
+  componentWillReceiveProps: function (newProps) {
+    var location = newProps.location.query.location;
 
     if (location && location.length > 0) {
       this.handleSearch(location);
